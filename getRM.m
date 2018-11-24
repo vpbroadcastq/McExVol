ad553c237d5053fff3094d3ffd9df7e700d84bac
Ben Knowles <40213817+nh2oh@users.noreply.github.com>
2018-11-24 07:50:08 -0600

    init

---
+++ getRM.m
@@ -0,0 +1,28 @@
+function R = getRM(Rv, theta)
+    % Rv is a vector specifying the axis of rotation, theta is the angle through
+    % which to rotate
+    if size(Rv,1) ~= 3 || size(Rv,2) ~= 1
+        error('The rotation vector must be 3x1');
+    end
+    if size(theta,1) ~= 1 || size(theta,2) ~= 1
+        error('The rotation angle theta must be a scalar');
+    end
+
+    if norm(Rv) ~= 0  % Can't norm the vector if it's [0;0;0] => divide by 0
+        Rv = Rv/norm(Rv);
+    end
+    Rx = Rv(1); Ry = Rv(2); Rz = Rv(3);
+
+    c = cos(theta);
+    s = sin(theta);
+    t = 1 - cos(theta);
+
+    % Build matrix for rotation about Rv
+    % Verified this is the same as is used in the MSL source
+    % CartesianGeometry::getRotationMatrix()
+    % RT ~ "rotation translation"
+    R = [t*Rx^2 + c, t*Rx*Ry - s*Rz, t*Rx*Rz + s*Ry];
+    R = [R; t*Rx*Ry + s*Rz, t*Ry^2 + c, t*Ry*Rz - s*Rx];
+    R = [R; t*Rx*Rz - s*Ry, t*Ry*Rz + s*Rx, t*Rz^2 + c];
+    
+end
---
+++ transPoints.m
@@ -0,0 +1,15 @@
+function [transPoint] = transPoints(p, Tv)
+    % Translates a set of N points p (3xN)
+    % p = [v1, v2, v3, ...vN], vi = [xi; yi; zi]
+    % Tv is a vector specifying the translation (the displacement, not the
+    % point to which we want to translate)
+
+    if size(p,1) ~= 3
+        error('The N points to be transformed must be specified as a 3xN matrix');
+    end
+    if size(Tv,1) ~= 3 || size(Tv,2) ~= 1
+        error('The translation vector must be 3x1');
+    end
+    
+    transPoint = bsxfun(@plus, p, Tv);
+end
